41234e8469edfa57ce9100958862e84ad3e89bfb
BestBlade <68151785+BestBlade@users.noreply.github.com>
2021-01-08 17:56:17 +0800

    Add files via upload

---
+++ leetcode_no117_填充每个节点的下一个右侧节点指针 II_w2.cpp
@@ -0,0 +1,132 @@
+﻿/* ------------------------------------------------------------------|
+给定一个二叉树
+
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+
+初始状态下，所有 next 指针都被设置为 NULL。
+
+ 
+
+进阶：
+
+你只能使用常量级额外空间。
+使用递归解题也符合要求，本题中递归程序占用的栈空间不算做额外的空间复杂度。
+ 
+
+示例：
+
+
+
+输入：root = [1,2,3,4,5,null,7]
+输出：[1,#,2,3,#,4,5,7,#]
+解释：给定二叉树如图 A 所示，你的函数应该填充它的每个 next 指针，以指向其下一个右侧节点，如图 B 所示。
+ 
+
+提示：
+
+树中的节点数小于 6000
+-100 <= node.val <= 100
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node-ii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*	迭代法
+*
+*	执行用时：12 ms, 在所有 C++ 提交中击败了97.51%的用户
+*	内存消耗：17.5 MB, 在所有 C++ 提交中击败了17.85%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+Node* connect(Node* root) {
+    if (!root) {
+        return root;
+    }
+    queue<Node*> q;
+    q.emplace(root);
+    while (!q.empty()) {
+        int len = q.size();
+        while (len--) {
+            Node* curr = q.front();
+            q.pop();
+            if (curr->left) {
+                q.emplace(curr->left);
+            }
+            if (curr->right) {
+                q.emplace(curr->right);
+            }
+            if (!len) {
+                break;
+            }
+            else {
+                curr->next = q.front();
+            }
+        }
+    }
+    return root;
+}
+
+int main() {
+    Node* n1 = new Node(1);
+    Node* n2 = new Node(2);
+    Node* n3 = new Node(3);
+    Node* n4 = new Node(4);
+    Node* n5 = new Node(5);
+    Node* n6 = new Node(6);
+    Node* n7 = new Node(7);
+    Node* n8 = new Node(8);
+    n1->left = n2;
+    n1->right = n3;
+    n2->left = n4;
+    n2->right = n5;
+    n3->right = n6;
+    n4->left = n7;
+    n6->right = n8;
+    Node* root = connect(n1);
+}
